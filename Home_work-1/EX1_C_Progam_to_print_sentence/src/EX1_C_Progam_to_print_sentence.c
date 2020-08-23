60f39387aca5d5059aa942d5c8b51cc6357c8667
Atta <atta-es@de001.itgr.net>
2020-08-23 20:19:23 +0200

    Write C Program to Print a Sentence

---
+++ Home_work-1/EX1_C_Progam_to_print_sentence/src/EX1_C_Progam_to_print_sentence.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+
+ Name        : EX1_C_Progam_to_print_sentence.c
+ Author      : Essam Atta
+ Version     :
+ Copyright   : Your copyright notice
+ Description : C_Progam_to_print_sentence
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("C Programming");
+	return 0;
+}
