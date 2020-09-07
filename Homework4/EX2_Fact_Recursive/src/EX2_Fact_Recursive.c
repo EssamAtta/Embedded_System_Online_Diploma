ad90108e4562dbe4bf34164021fc2fc867fa6e39
Atta <atta-es@de001.itgr.net>
2020-09-07 17:18:51 +0200

    EX2_Fact_Recursive

---
+++ Homework4/EX2_Fact_Recursive/src/EX2_Fact_Recursive.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : EX2_Fact_Recursive.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int fact (int n);
+
+int main(void) {
+	int x;
+	int factorial = 0;
+	printf("Enter postive number:");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d",&x);
+	factorial = fact (x);
+	printf("factorial of %d is %d",x,factorial);
+
+	return EXIT_SUCCESS;
+}
+
+
+int fact (int n){
+	static int facto = 1;
+
+	facto = facto * n;
+	n--;
+	if (n>0) return fact(n);
+	else return facto;
+	}
+
