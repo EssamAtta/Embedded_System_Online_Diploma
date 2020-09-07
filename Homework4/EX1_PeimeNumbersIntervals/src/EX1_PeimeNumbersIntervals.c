1c02575d1e2c01b21d03aba538b2ad8f5caa5b81
Atta <atta-es@de001.itgr.net>
2020-09-07 17:17:47 +0200

    EX1_PeimeNumbersIntervals

---
+++ Homework4/EX1_PeimeNumbersIntervals/src/EX1_PeimeNumbersIntervals.c
@@ -0,0 +1,51 @@
+/*
+ ============================================================================
+ Name        : EX1_PeimeNumbersIntervals.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+// Function prototype
+void Prime_Numbers(int a, int b);
+
+int main(void) {
+	int x , y;
+	printf("Enter two Numbers(intervels):");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d  %d",&x,&y);
+	Prime_Numbers(x, y);
+	return EXIT_SUCCESS;
+}
+
+void Prime_Numbers(int a, int b)
+{
+int flag= 0;
+
+	printf("Prime Numbers between %d and %d are :", a,b);
+	while (a <= b)
+	{
+		for (int i=2; i<=9;i++){
+			if (a%i == 0)
+			{
+				flag = 1;
+			}
+
+		}
+		if (flag == 1)
+		{
+			flag = 0;
+			a++;
+		}
+		else
+		{
+			printf("%d ",a);
+			a++;
+		}
+	}
+}
